e3eaa0a73ec907ebc09815d6cbccbe159ca1e1a4
pseminatore <pseminatore@gmail.com>
2016-05-20 09:06:43 -0700

    https://github.com/pseminatore/jnk.gt

--- Dodge_Bot.java
+++ Dodge_Bot.java
@@ -4,7 +4,7 @@ import robocode.util.*;
 import java.awt.Color;
 
 /**
- * Dodge_Bot - a robot by (Jack Kimball)
+ * Dodge_Bot - a robot by (Jack Kimball and Patrick and Connor)
  */
 public class Dodge_Bot extends AdvancedRobot {
 	
